db0d7166b65996b47931a64cb7fd2acc49cc4bf9
ElliotTapperKarlsson <elliottk@kth.se>
2024-01-17 14:50:42 +0100

    Update NumLabb_1.m

--- NumLabb_1.m
+++ NumLabb_1.m
@@ -2,9 +2,22 @@
 % Uppg 1 - Linjärt ekvationssystem
 
 %Vi ska lösa ekvationssystemet Ax=b
-%Dedfinierar matris A
+%Deffinierar matris A
 A=[1 2 3 0;
   0 4 5 6;
   1 1 -1 0;
+<<<<<<< Updated upstream
   1 1 1 1];
 b
+=======
+  1 1 1 1]
+
+b=[7 6 5 4]'
+
+%Definierar x-vektorn
+x=[x1 x2 x3 x4]'
+
+
+
+
+>>>>>>> Stashed changes
