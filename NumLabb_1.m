95ad3560924095313a0e55811b4d0f7a90c6f68e
ElliotTapperKarlsson <elliottk@kth.se>
2024-01-17 14:23:09 +0100

    tewzt

---
+++ NumLabb_1.m
@@ -0,0 +1,12 @@
+%______________Numme Labb 1_________________
+% Uppg 1 - Linjärt ekvationssystem
+
+%Vi ska lösa ekvationssystemet Ax=b
+%Dedfinierar matris A
+A=[1 2 3 0;
+  0 4 5 6;
+  1 1 -1 0;
+  1 1 1 1];
+b
+
+
